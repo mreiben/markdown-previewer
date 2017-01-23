a1e49c9a47b60fe7cead9147e3687e9a18b19531
Jason Eiben <jason.eiben@gmail.com>
2017-01-23 09:44:03 -0500

    fixed use of marked package

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 var marked = require('marked');
 
+const text = marked("I am using __markdown__.");
+
+function createMarkup(foo){
+  return {__html: marked(foo)};
+}
+
 class App extends Component {
-  //const text = marked('I am using __markdown__.');
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React - Test!!!</h2>
-          
+          <div dangerouslySetInnerHTML={createMarkup("I am using `markdown`.")}/>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
