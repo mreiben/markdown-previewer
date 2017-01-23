a353900d1739a7fcc75517675dae693fce8be01a
Jason Eiben <jason.eiben@gmail.com>
2017-01-23 00:01:34 -0500

    test marked package

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css';
 var marked = require('marked');
 
 class App extends Component {
-  const text = marked('I am using __markdown__.');
+  //const text = marked('I am using __markdown__.');
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React - Test!!!</h2>
-          <h2>{text}</h2>
+          
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
