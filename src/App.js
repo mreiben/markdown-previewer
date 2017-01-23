520ecd8e06b4e2f5a31f5769b4d49d3c0d5247c5
Jason Eiben <jason.eiben@gmail.com>
2017-01-23 12:41:13 -0500

    fixed React state updates

--- src/App.js
+++ src/App.js
@@ -3,24 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 var marked = require('marked');
 
-const text = marked("I am using __markdown__.");
-
 function createMarkup(foo){
   return {__html: marked(foo)};
 }
 
+//<div dangerouslySetInnerHTML={createMarkup("I am using `markdown` so __boom__.")}/>
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: 'Enter __markup__ *here* to see a preview on the right!\r\rcreated by [mreiben](https://github.com/mreiben)',
+      htmlText: createMarkup('Enter __markup__ *here* to see a preview on the right!\r\rcreated by [mreiben](https://github.com/mreiben)')
+    }
+  }
+
+  onInputChange(term){
+    this.setState({ text: term, htmlText: createMarkup(term)});
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React - Test!!!</h2>
-          <div dangerouslySetInnerHTML={createMarkup("I am using `markdown`.")}/>
+        <div className="section">
+          <textarea
+            type="text"
+            value={ this.state.text }
+            onChange={ event => this.onInputChange(event.target.value) }
+          ></textarea>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div
+          className="section"
+          dangerouslySetInnerHTML={this.state.htmlText}>
+        </div>
       </div>
     );
   }
